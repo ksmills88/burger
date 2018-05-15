d443f98ccaf929bec193a6c12a866267ab4ba62f
kmills <ksmills82@gmail.com>
2018-05-15 18:30:50 -0400

    updated js

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,14 @@
 -- burgers_db
 -- burgers table with fields: id, burger_name, devoured (boolean)
+
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
 -- at least 3 burger entries
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quinoa', FALSE);
+
